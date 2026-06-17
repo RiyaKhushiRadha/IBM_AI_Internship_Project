1e84d71e6330bf1dc769e5ff668850b5754a8f64
Riya <khushipadhnahai@gmail.com>
2026-06-17 21:30:34 +0530

    Added skill extraction and role recommendation

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+
 from utils.parser import extract_text_from_pdf
+from utils.skill_extractor import extract_skills
+from utils.role_recommender import recommend_roles
+from utils.skill_extractor import extract_skills
 
 st.set_page_config(
     page_title="Smart Career Assistant",
@@ -19,12 +23,43 @@ if uploaded_file is not None:
 
     st.success("Resume uploaded successfully!")
 
+    # Resume Parsing
     resume_text = extract_text_from_pdf(uploaded_file)
 
-    st.subheader("Extracted Resume Text")
+    # st.subheader("📄 Extracted Resume Text")
 
-    st.text_area(
-        "Resume Content",
-        resume_text,
-        height=300
-    )
+    # st.text_area(
+    #     "Resume Content",
+    #     resume_text,
+    #     height=300
+    # )
+
+    # Skill Extraction
+    skills = extract_skills(resume_text)
+
+    st.subheader("🛠 Extracted Skills")
+
+    if skills:
+
+        for skill in skills:
+            st.write(f"• {skill}")
+
+    else:
+        st.warning("No skills detected.")
+
+    # Role Recommendation
+    recommendations = recommend_roles(skills)
+
+    st.subheader("🎯 Recommended Roles")
+
+    if recommendations:
+
+        for role, score in recommendations:
+            st.write(
+                f"**{role}** → Match Score: {score:.0f}%"
+            )
+
+    else:
+        st.warning(
+            "No matching roles found."
+        )
